10344ff4ac1a350e369ebd54c0e2989099ecb7a1
JennyShalai <jenny.shalai@gmail.com>
2016-06-20 17:20:33 -0400

    done

--- BlackJack/FISAppDelegate.m
+++ BlackJack/FISAppDelegate.m
@@ -1,11 +1,18 @@
 //  FISAppDelegate.m
 
 #import "FISAppDelegate.h"
+#import "FISBlackjackPlayer.h"
 
 @implementation FISAppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    
+    
+    FISBlackjackPlayer *player = [[FISBlackjackPlayer alloc] initWithName:@"Jenny"];
+    NSLog(@"%@", player.description);
+    
+    
     return YES;
 }
 
